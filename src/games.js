658d91a44bf7f04202028317de66b87d6b388459
Jamie <jamie.conn65@gmail.com>
2019-04-08 22:48:03 +0100

    Add more games

--- src/games.js
+++ src/games.js
@@ -16,7 +16,7 @@ export default [
     answer: 13,
     operation: 'multiply',
     instruction: "Choose two cards that make the sentence correct",
-    question_text: ["13 times", "is"]
+    question_text: ["13 x", "is"]
   },
   {
     name: "Number bonds to 10",
@@ -24,7 +24,25 @@ export default [
     values: [0, 10, 1, 9, 2, 8, 3, 7, 4, 6, 5, 5],
     answer: 10,
     operation: 'add',
-    instruction: "Choose two cards that make the sentence correct",
+    instruction: "Choose two cards that add to 10",
     question_text: ["add", "is 10"]
+  },
+  {
+    name: "Negative number bonds",
+    question_type: "_*_=n",
+    values: [-5, 10, 7, -2, 1, 4, -1, 6, -4, 9, -3, 8],
+    answer: 5,
+    operation: 'add',
+    instruction: "Choose two cards that add to make 5",
+    question_text: ["add", "is 5"]
+  },
+  {
+    name: "Multiplying negatives",
+    question_type: "_*_=n",
+    values: [-1, 12, 1, -12, 3, -4, -3, 4, 2, -6, 2, -6],
+    answer: -12,
+    operation: 'multiply',
+    instruction: "Choose two numbers that multiply to give -12",
+    question_text: ["times", "is -12"]
   },
 ]
