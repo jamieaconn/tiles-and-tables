6b1bec3cc1af0dbc4eabf8e23bfa1c3623447205
Jamie <jamie.conn65@gmail.com>
2019-04-10 21:28:39 +0100

    build

---
+++ build/precache-manifest.da3bc598c5ad9b88080185d2c05b7040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "87620efe0488b24b5df1",
+    "url": "/static/css/main.18ea173b.chunk.css"
+  },
+  {
+    "revision": "87620efe0488b24b5df1",
+    "url": "/static/js/main.f4d1237d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aadf89f77144d345dc68",
+    "url": "/static/css/2.7b2fb5bf.chunk.css"
+  },
+  {
+    "revision": "aadf89f77144d345dc68",
+    "url": "/static/js/2.08945968.chunk.js"
+  },
+  {
+    "revision": "5c3505d832b2b2940b96bb8c0470f9a7",
+    "url": "/index.html"
+  }
+];
